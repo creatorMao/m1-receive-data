36faa725e0df6e859b6df9e7fa25be001d9dd771
Zhiwu Mao <zwmao@outlook.com>
2023-02-02 22:21:09 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,6 @@
+把斐讯m1数值发送到[信息中转中心](https://github.com/creatorMao/news-center)
+
+1. 获取计划运行当前程序的设备的内网ip
+2. 修改app.js中第四行的HOST值
+3. 劫持域名aircat.phicomm.com到刚才的ip
+4. 启动程序npm run start
---
+++ app.js
@@ -0,0 +1,52 @@
+import net from 'net';
+import axios from 'axios'
+
+const HOST = "192.168.1.201";
+const PORT = 9000;
+const newsCenterUrl = 'http://xxxx:3000/news/add?token=1'
+
+const server = net.createServer();
+server.listen(PORT, HOST);
+
+server.on("connection", (socket) => {
+
+  console.log(`connected:${socket.remoteAddress}:${socket.remotePort}`);
+  console.log(`local:${socket.localAddress}:${socket.localPort}`);
+
+  // socket.write("服：你好客户端");
+
+  socket.on("data", (data) => {
+    const content = data.toString().replaceAll(' ', '');
+    // demo: ����#�N{"humidity":"44.95","temperature": "19.01", "value": "9", "hcho": "50" }�#END#
+    const dataArray = content.split('#END#');
+    if (dataArray.length > 0) {
+      dataArray.forEach((dataItem) => {
+        const patternRes = /\{\s*(\S+)\s*\}/.exec(dataItem)
+        if (patternRes) {
+          // console.log(`原始数据：${dataItem}`);
+          const parsedData = JSON.parse(patternRes[0])
+          console.log(`处理后的数据：${JSON.stringify(parsedData)}`);
+
+          axios({
+            method: 'post',
+            url: newsCenterUrl,
+            params: {
+              groupId: 'm1',
+              content: JSON.stringify(parsedData)
+            }
+          }).then((res) => {
+            console.log(res.data);
+            console.log('--------------------');
+          });
+        }
+      })
+    }
+  });
+
+  socket.on("end", (data) => {
+    console.log(`客户端${socket.remoteAddress}:${socket.remotePort}已断连`);
+  });
+
+});
+
+console.log(`server listen on ${HOST}:${PORT}`);
